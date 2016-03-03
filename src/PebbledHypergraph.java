0d74fc6e8c94be29a02a74a2a4d3590389861b11
Christopher Schulstad <christopherschulstad@gmail.com>
2016-03-03 01:06:38 -0600

    Added files via upload

--- src/PebbledHypergraph.java
+++ src/PebbledHypergraph.java
@@ -16,6 +16,33 @@ public class PebbledHypergraph
         nodes = nodeList;
     }
     
+    public void pebbleNode(int id){
+    	nodes.get(id).activate();
+    }
+    
+    //*****************************************************
+    //loop over source nodes to see if each are pebbled
+    public boolean isFullyPebbled(PebbledHyperedge edge){
+    	if (edge.isPebbled()){
+    		return true;
+    	}
+    	for(Integer src : edge.sourceNodes){
+    		if (!nodes.get(src).isPebbled()){
+    			return false;
+    		}
+    	}
+    	edge.pebble();
+    	return true;
+    }
+    
+    public boolean isNodePebbled(int id){
+    	return nodes.get(id).isPebbled();
+    }
+    
+    public ArrayList<PebbledHyperedge> getEdges(int id){
+    	return nodes.get(id).outEdges;
+    }
+    
     @Override
     public String toString()
     {
